3ecfca8d22cc54a79e8aa88b92ffd36691bf3362
Alex <al.korotkov@intranet-sintek.net>
2022-06-09 13:06:10 +0300

    refactoring + new js script initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 //Блок объявления переменных
 const page = document.querySelector('.page');
 
-const popupEdit = page.querySelector('.popup_edit');
-const popupAdd = page.querySelector('.popup_add');
-const popupCard = page.querySelector('.popup_card');
+const popupEdit = page.querySelector('.popup_type_edit');
+const popupAdd = page.querySelector('.popup_type_add');
+const popupCard = page.querySelector('.popup_type_card');
 
 const formEdit = popupEdit.querySelector('.form_profile_edit');
 const formAdd = popupAdd.querySelector('.form_card_add');
@@ -21,20 +21,26 @@ const urlInput = formAdd.querySelector('.form__input_content_url');
 const popupCardImage = popupCard.querySelector('.card-scale__image');
 const popupCardCaption = popupCard.querySelector('.card-scale__caption');
 
+const cardsList = page.querySelector('.elements__cards');
+const cardTemplate = page.querySelector('#cardTemplate').content;
 
+//Функция открытия попапа
+function openPopup(popupElement) {
+  popupElement.classList.add('popup_opened');
+};
 
 //Функция открытия попапа редактирования профиля
 function openPopupEdit() {
   nameInput.value = nameProfile.textContent;  //заполняем поля ввода данными из профиля
   jobInput.value = jobProfile.textContent;
-  popupEdit.classList.add('popup_opened');
+  openPopup(popupEdit);
 };
 
 //Функция открытия попапа добавления карточки
 function openPopupAdd() {
-  placeInput.value = '';  //обнуляем поля ввода если там что то есть
-  urlInput.value = '';
-  popupAdd.classList.add('popup_opened');
+  //placeInput.value = '';  //обнуляем поля ввода если там что то есть (перенесено в Обработчик добавления новой карточки)
+  //urlInput.value = '';
+  openPopup(popupAdd);
 };
 
 //Функция открытия попапа карточки
@@ -43,16 +49,63 @@ function openPopupCard(evt) {
   //console.log(btnCardImage.closest('.card__image').src);
   //console.log(btnCardImage.nextElementSibling.firstElementChild.textContent);
   popupCardImage.src = btnCardImage.closest('.card__image').src;
+  popupCardImage.alt = 'попап ' + btnCardImage.closest('.card__image').alt;
   popupCardCaption.textContent = btnCardImage.nextElementSibling.firstElementChild.textContent;
-  popupCard.classList.add('popup_opened');
+  openPopup(popupCard);
 };
 
 //Функция закрытия любого попапа
 function closePopup(evt) {
-  const btnClosePopup = evt.target; //ловим элемент кнопку закрытия попапа
-  //console.log(btnClosePopup);
-  btnClosePopup.closest('.popup_opened').classList.remove('popup_opened');
+  const targetItem = evt.target; //ловим элемент по которому сработало событие закрытия попапа
+  //console.log(targetItem);
+  targetItem.closest('.popup_opened').classList.remove('popup_opened');
 };
+
+//Функция для лайков
+function likeCard(evt) {
+  const btnCardLike = evt.target; //ловим элемент кнопку лайка
+  //console.log(btnCardLike);
+  btnCardLike.classList.toggle('card__like_active');
+};
+
+//Функция для удаления карточки
+function deleteCard(evt) {
+  const btnCardDelete = evt.target; //ловим элемент кнопку удаления карточки
+  btnCardDelete.closest('.card').remove();
+};
+
+//Функция создания карточек по умолчанию
+function initCards() {
+  for (let i=0; i<initialCards.length; i++)
+  {
+    createCard(i);
+    insertCard(i);
+  }
+};
+
+//Функция создания карточки на основе данных из массива
+function createCard(i) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = initialCards[i].link;
+  cardElement.querySelector('.card__image').alt = 'фотография ' + (i+1);
+  cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+  const btnCardLike = cardElement.querySelector('.card__like');
+  btnCardLike.addEventListener('click', likeCard);
+  const btnCardDelete = cardElement.querySelector('.card__delete');
+  btnCardDelete.addEventListener('click', deleteCard);
+  const btnCardImage = cardElement.querySelector('.card__image');
+  btnCardImage.addEventListener('click', openPopupCard);
+  //console.log(cardElement);
+  return cardElement;
+}
+
+//Функция вставки карточки в начало списка
+function insertCard(i) {
+  cardsList.prepend(createCard(i));
+};
+
+
+
 
 //Обработчик отправки формы редактирования профиля
 function formEditSubmitHandler (evt) {
@@ -71,36 +124,12 @@ function formAddSubmitHandler (evt) {
     link: urlInput.value
   });
   //console.log(initialCards);
-  createCard(0);
+  createCard(0);  //создали карточку из 0ого элемента массива
+  insertCard(0);  //вставили эту же карточку
   closePopup(evt);
+  formAdd.reset();  //Очищаем поля формы
 };
 
-//Функция для лайков
-function likeCard(evt) {
-  const btnCardLike = evt.target; //ловим элемент кнопку лайка
-  btnCardLike.closest('.card__like').classList.toggle('card__like_active');
-};
-
-//Функция для удаления карточки
-function deleteCard(evt) {
-  const btnCardDelete = evt.target; //ловим элемент кнопку удаления карточки
-  btnCardDelete.closest('.card').remove();
-};
-
-//Функция создания карточки на основе данных из массива
-function createCard(i) {
-  const cardTemplate = page.querySelector('#cardTemplate').content;
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = initialCards[i].link;
-  cardElement.querySelector('.card__title').textContent = initialCards[i].name;
-  const btnCardLike = cardElement.querySelector('.card__like');
-  btnCardLike.addEventListener('click', likeCard);
-  const btnCardDelete = cardElement.querySelector('.card__delete');
-  btnCardDelete.addEventListener('click', deleteCard);
-  const btnCardImage = cardElement.querySelector('.card__image');
-  btnCardImage.addEventListener('click', openPopupCard);
-  cards.prepend(cardElement);
-}
 
 
 
@@ -125,37 +154,6 @@ formEdit.addEventListener('submit', formEditSubmitHandler);
 formAdd.addEventListener('submit', formAddSubmitHandler);
 
 
-
-
-//Создание 6ти карточек по умолчанию
-const cards = page.querySelector('.elements__cards');
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 /*
 for (let i=0; i<initialCards.length; i+=1)
@@ -185,7 +183,6 @@ for (let i=0; i<initialCards.length; i+=1)
   cardsItemDescription.append(cardsItemLike);
 };
 */
-for (let i=0; i<initialCards.length; i++)
-{
-  createCard(i);
-};
+
+//Создаем карточки по умолчанию
+initCards();
